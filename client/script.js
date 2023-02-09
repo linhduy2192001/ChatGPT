ab9de8e7b3640b19782a560d2462da3cec67f0d5
Linh Duy <duypnl219@gmail.com>
2023-02-09 21:20:45 +0700

    update

--- client/script.js
+++ client/script.js
@@ -65,7 +65,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-6mps.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
